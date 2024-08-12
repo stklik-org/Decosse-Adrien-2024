ad76868d10a7c418875417118c2b65152fd4a584
Didictateur <decosse.adrien@gmail.com>
2024-08-12 17:38:23 +0200

    added comments on manual_calibration.py

--- compute/manual_calibration.py
+++ compute/manual_calibration.py
@@ -5,39 +5,41 @@ selected_point = None
 L_target = []
 img = None
 
-def select_point(event, x, y, flags, param):
+## when calibrating the camera, the position of the markers on the image can be manually selected
+
+def select_point(event, x, y, flags, param): # processing the modification of the position of one marker
     global selected_point, L_target
-    if event == cv2.EVENT_LBUTTONUP and selected_point is None:
+    if event == cv2.EVENT_LBUTTONUP and selected_point is None: # select the closest point when clicking
         for i, point in enumerate(L_target):
-            if abs(x - point[0]) < 10 and abs(y - point[1]) < 10:
+            if abs(x - point[0]) < 10 and abs(y - point[1]) < 10: # but only if close enough
                 selected_point = i
                 break
-    elif event == cv2.EVENT_LBUTTONUP:
+    elif event == cv2.EVENT_LBUTTONUP: # click again to stop modifying the point
         selected_point = None
-    elif event == cv2.EVENT_MOUSEMOVE:
+    elif event == cv2.EVENT_MOUSEMOVE: # move the point when moving the mouse
         if selected_point is not None:
             L_target[selected_point] = (x, y)
     redraw_lines()
 
-def redraw_lines():
+def redraw_lines(): # redraw the line after modification
     global L_target, img, selected_point
     img_copy = img.copy()
-    if selected_point is not None:
+    if selected_point is not None: # zoom when a point is selected in order to get a better precision
         x, y = L_target[selected_point]
         zoom_size = 100  # size of the zoomed region
         try:
             zoom_img = img[max(0, y-zoom_size//2):min(img.shape[0], y+zoom_size//2), max(0, x-zoom_size//2):min(img.shape[1], x+zoom_size//2)]
             zoom_img = cv2.resize(zoom_img, (zoom_size*2, zoom_size*2))
             img_copy[max(0, y-zoom_size):min(img.shape[0], y+zoom_size), max(0, x-zoom_size):min(img.shape[1], x+zoom_size)] = zoom_img
-        except:
+        except: # in the case the zoomed square exceeds the image
             pass
     cv2.line(img_copy, (int(L_target[0][0]), int(L_target[0][1])), (int(L_target[1][0]), int(L_target[1][1])), (0, 0, 255), 1)
     cv2.line(img_copy, (int(L_target[1][0]), int(L_target[1][1])), (int(L_target[3][0]), int(L_target[3][1])), (0, 0, 255), 1)
     cv2.line(img_copy, (int(L_target[3][0]), int(L_target[3][1])), (int(L_target[2][0]), int(L_target[2][1])), (0, 0, 255), 1)
     cv2.line(img_copy, (int(L_target[2][0]), int(L_target[2][1])), (int(L_target[0][0]), int(L_target[0][1])), (0, 0, 255), 1)
-    cv2.imshow('Matched Areas', img_copy)
+    cv2.imshow('Matched Areas', img_copy) # and drawing it, of course
 
-def manual_calibration(img_, L_target_):
+def manual_calibration(img_, L_target_): # open the image and proceed to the manual calibration
     global selected_point, L_target, img
     selected_point = None
     L_target = L_target_
